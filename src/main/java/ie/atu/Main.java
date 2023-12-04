df8a946c4bf259b56edd5441480b9c0ac9e603cc
Aidan Malone <G00423306@atu.ie>
2023-12-04 16:35:50 +0000

    set up repo

---
+++ src/main/java/ie/atu/Main.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+
+public class Main {
+    public static void main(String[] args) {
+      Person myPerson = new Person("aidan", "ennistymon", "234");
+        System.out.println(myPerson.toString());
+
+        Customer myCustomer = new Customer();
+    }
+}
---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,52 @@
+package ie.atu;
+
+import java.util.Scanner;
+public class Person {
+    private String name;
+    private String userAddress;
+    private String userNumber;
+
+
+    public Person() {
+        this.name = "";
+        this.userAddress = "";
+        this.userNumber = "";
+    }
+
+    public Person(String name) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", userAddress='" + userAddress + '\'' +
+                ", userNumber='" + userNumber + '\'' +
+                '}';
+    }
+}
